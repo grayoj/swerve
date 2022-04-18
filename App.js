bc005239383322ea4061acba43d5af035bdb0602
Gerald Maduabuchi <mgeraldoj07@gmail.com>
2022-04-18 13:38:41 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text></Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+});
---
+++ components/shortTabs.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const shortTabs = () => {
+  return (
+    <View>
+      <Text>shortTabs</Text>
+    </View>
+  )
+}
+
+export default shortTabs
